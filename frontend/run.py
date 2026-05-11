6d23e3389fb2b8a24727006686599930a07d59a7
thiagosak-dot <thiago.sak@guarapuava.pr.gov.br>
2026-05-11 17:28:30 -0300

    Realizado mudanças no template: -Realizado mudanças para deixar o texto estruturado.

--- frontend/run.py
+++ frontend/run.py
@@ -4,7 +4,7 @@ from app import app
 if __name__ == '__main__':
     try:
         print("Iniciando o servidor...")
-        serve(app, host='127.0.0.1', port=5000, threads=4, develop=True)
+        serve(app, host='127.0.0.1', port=5000, threads=4)
         
     except Exception as e:
         print(f"Erro ao iniciar o servidor: {e}")
